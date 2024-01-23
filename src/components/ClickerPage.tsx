64937c36990c901d12de8e61ceb724a063f6e291
Radz Santillan <116485181+radz22@users.noreply.github.com>
2024-01-24 00:47:24 +0800

    create alert button and result

--- src/components/ClickerPage.tsx
+++ src/components/ClickerPage.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import clickImage from '../assets/photo.png'
+import { IoCloseOutline } from "react-icons/io5";
+import { BsMouse3Fill } from "react-icons/bs";
+import { MdCelebration } from "react-icons/md";
+
 interface Records  {
     id:number
     second:number,
     clicks:number,
     date:string
+    name:string
 }
 
 
@@ -15,14 +20,14 @@ const ClickerPage = () => {
     const [intervalId, setIntervalId] = useState<number | null>(null);
     const [count, setCount] = useState(0);
     const [currentTime, setCurrentTime] = useState<string>('');
-
-
-
+    const [displayAlertUser, setDisplayAlertUser] = useState<boolean>(false)
+    const [result, setResult] = useState<boolean>(false)
+    const [user, setUser] = useState<string>("")
     const [click, setClick] = useState<number >(0)
     const [record, setRecord] = useState <Records[]>([])
+    const [recordSeconds, setRecordSeconds] = useState<number>(0)
 
 
-    const [recordSeconds, setRecordSeconds] = useState<number >(0)
 
     function handleSeconds10s(){
 
@@ -44,7 +49,6 @@ const ClickerPage = () => {
     }
 
 
-  
     function handleSeconds5s(){
 
      
@@ -86,12 +90,23 @@ const ClickerPage = () => {
     }
  
 
+    function handleClose (){
+        setDisplayAlertUser(!displayAlertUser)
+    }
     /* click button to add one */
     function handleCLickAdd (){
-
+       
         
+           if(count == 0){
+            setDisplayAlertUser(true)
+           }else{
             setClick((prev) => prev + 1)
+           }
      
+if(user == ""){
+    setClick(0)
+    setDisplayAlertUser(true)
+}
 
 
         const time = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
@@ -103,29 +118,43 @@ const ClickerPage = () => {
     }
 
 
+    function handleResultClose (){
+        setResult(!result)
+    }
 /* if count is 0 and nested if to stop a count */
 if(count == 0){
+
+
     if (intervalId !== null) {
         clearInterval(intervalId);
         setIntervalId(null); 
-        setRecord([...record, {id:Date.now(), second:recordSeconds, clicks:click, date:currentTime}])
-        
+      
+        if(user.length  >= 1){
+            setResult(true)
+            setRecord([...record, {id:Date.now(), second:recordSeconds, clicks:click, date:currentTime, name:user}])
+        }   
       }
       
+}else if(count < 0){
+    setCount(0)
 }
+
 
 /* reset . go back to 0 value */
 function handleReset (){
     setClick(0)
+    setCount(0)
 }
 
+console.log(click)
 
-console.log
+
 
 
     
   return (
- <div className='h-auto w-full flex items-center justify-center flex-col '>
+ <>
+ <div className={`h-auto w-full flex items-center justify-center flex-col relative z-0 ${displayAlertUser ? "bg-slate-300		" : "bg-white" }`}>
 
 
 
@@ -141,7 +170,16 @@ console.log
 
 <div className='mt-12 w-full'>
 <div>
-    <h1 className='text-center text-2xl	font-semibold	'>Put Your Finger-clicking speed to the test</h1>
+    <h1 className='text-center text-2xl	font-semibold'>Put Your Finger-clicking speed to the test</h1>
+</div>
+<div className='flex items-center justify-center  mt-10 w-full	'>
+    
+    <input type='text'
+    value={user}
+    onChange={(e) => setUser(e.target.value)}
+    placeholder='Enter Your Name'
+    className='w-1/4 py-3 px-3 text-lg	border-2 border-sky-600 bg-slate-100		rounded-md	'
+    />
 </div>
 <div className='flex items-center justify-center gap-6 mt-16'>
 
@@ -169,7 +207,7 @@ console.log
 
        </div>
        <div>
-       <p className='text-right text-black text-xl		'>Time: {count}</p>
+       <p className='text-right text-black text-xl	'>Time: {count}</p>
        </div>
     </div>
 
@@ -197,11 +235,12 @@ console.log
 
 {
    record.map((item) => (
-    <div className='bg-[#d2d2d2] px-3 py-3 w-4/5	mb-10'>
+    <div className='bg-slate-200		 px-3 py-3 w-4/5	mb-10'>
 
 
         <div className='flex items-center justify-between'>
           <div>
+          <p>Name: <span className='text-lg'>{item.name.toUpperCase()}</span></p>
             <p>Score: <span className='text-lg'>{item.clicks}</span></p>
             </div>
             <div>
@@ -219,7 +258,78 @@ console.log
 
 </div>
 </div>
+
+
+<div className={`${displayAlertUser ? "block" : "hidden"} absolute  z-10   top-[48%] w-full flex items-center justify-center flex-col`}>
+<div className='bg-[#cb1111] rounded-md		 px-10 py-10 w-[40%] h-44		 '>
+
+<div className='flex items-end justify-end mb-2'>
+    <h1 className='font-semibold	px-2 py-1 text-2xl text-black	 bg-sky-300	mt cursor-pointer	rounded-md	' onClick={handleClose}><IoCloseOutline /></h1> 
+</div>
+<div className='flex items-center justify-center gap-2 mt-5'>
+<div>
+<h1 className='text-slate-300	 text-xl	font-semibold font-serif
+
+	'>pls input your Name or Select your Time before you Start!!! </h1>
+
+</div>
+<div>
+<h1 className='text-white text-2xl		font-semibold'><BsMouse3Fill />
+</h1>
+</div>
+
+</div>
+</div>
+</div>
+
+
+<div className={`${result ? "block" : "hidden"} absolute  z-10   top-[44%] w-full flex items-center justify-center flex-col`}>
+<div className='bg-sky-600	rounded-md	 px-10 py-10 w-[40%] h-auto		 '>
+
+<div className='flex items-end justify-end mb-2'>
+    <h1 className='font-semibold	px-2 py-1 text-2xl text-black	 bg-sky-300	mt cursor-pointer	rounded-md	' onClick={handleResultClose}><IoCloseOutline /></h1> 
+</div>
+<div className='flex items-center justify-center gap-2 mt-5'>
+<div>
+<h1 className='text-white	 text-3xl		font-semibold font-serif
+	'>Congrats!!! </h1>
+</div>
+<div>
+<h1 className='text-white text-2xl		font-semibold'><MdCelebration /></h1>
+</div>
+</div>
+<div className='text-center mt-5 flex items-center justify-center '>
+    
+    <div className='w-1/2	'>
+  <div>
+  <h1 className='text-xl	text-slate-300'>Name: <span className='text-2xl text-white'>{user.toUpperCase()}</span></h1>
+    </div>  
+
+    <div>
+  <h1 className='text-xl	text-slate-300'>Seconds: <span className='text-2xl text-white'>{recordSeconds}s</span></h1>
+    </div> 
+ 
+
+    </div>
+
+    <div className='w-1/2		 '>
+  <div >
+  <h1 className='text-xl	text-slate-300'>Score: <span className='text-2xl text-white'>{click}</span></h1>
+    </div>  
+
+    <div >
+  <h1 className='text-xl	text-slate-300'>Time: <span className='text-2xl text-white'>{currentTime}</span></h1>
+    </div> 
+ 
+    </div>
+   
+    
+</div>
+</div>
+</div>
  </div>
+
+ </>
   )
 }
 
